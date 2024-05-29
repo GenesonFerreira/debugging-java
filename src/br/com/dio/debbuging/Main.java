f9c67935a3cf65331a4c7030cf7904b2bf5af394
GenesonFerreira <jgeneson@gmail.com>
2024-05-29 17:38:48 -0300

    Debbuging Java

--- src/br/com/dio/debbuging/Main.java
+++ src/br/com/dio/debbuging/Main.java
@@ -7,20 +7,20 @@ public class Main {
         System.out.println("Finalizou do programa no método main.");
     }
 
-    static void a() {
+    public static void a() {
         System.out.println("Entrou no método a.");
         b();
         System.out.println("Finalizou o método a.");
     }
 
-    static void b() {
+    public static void b() {
         System.out.println("Entrou no método b.");
         for(int i = 0; i <= 4; i++) System.out.println(i);
         c();
         System.out.println("Finalizou o método b.");
     }
 
-    static void c(){
+    public static void c(){
         System.out.println("Entrou no método c.");
         //Thread.dumpStack();
         System.out.println("Finalizou o método c.");
